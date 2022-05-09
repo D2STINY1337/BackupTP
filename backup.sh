8892c472901cde9185459c71a260b7e01cc299c4
D2STINY1337 <lucas.nogales1990@gmail.com>
2022-05-09 02:01:53 -0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,4 +2,4 @@
 Fecha=$(date +%y-%m-%d)
 hora=$(date +%H:%M)
 username="$(whoami)"
-tar -cvf /home/username/backup_$fecha'_'$hora /home/lnogales
+tar -cvf /home/username/backup_$fecha'_'$hora /home/username
